192df84fc74126d9ab2bc973845d6928692dfe25
steve12512 <86954812+steve12512@users.noreply.github.com>
2024-08-11 16:53:02 +0300

    ..

--- e-commerce-app/src/main/java/com/ecommerce/app/Workstation.java
+++ e-commerce-app/src/main/java/com/ecommerce/app/Workstation.java
@@ -21,6 +21,6 @@ public class Workstation extends Personal_Computer {
 
     @Override
     public String toString() {
-        return "Workstation \n" + super.toString() + "\n\nOperating System: " + os;
+        return "Workstation; \n" + super.toString() + "\n\nOperating System: " + os;
     }
 }
